e1bf3af66ed0067027b2511b20185ee0c32b52fe
Jesús Arribi <arribi@boxer.local>
2024-02-05 18:20:54 +0000

    All the methods implemented

--- index.js
+++ index.js
@@ -1,12 +1,19 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const dns = require('dns');
+const { URL } = require('url');
+const bodyParser = require("body-parser");
 const app = express();
+
+// I store de URLs in an array
+const addresses = [];
 
 // Basic Configuration
 const port = process.env.PORT || 3000;
 
 app.use(cors());
+app.use(bodyParser.urlencoded({ extended: false }));
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
@@ -19,31 +26,48 @@ app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-addresses = [];
-
 app.post('/api/shorturl', (req, res) => {
   const url = req.body.url;
-  const short_url = addresses.length + 1;
 
-  dns.lookup(url, (err, addresses,) => {
-    if (err) {
-      res.json({
-        error: 'invalid url'
-      });
-    } else {
-      addresses.push({ original_url: url, short_url: short_url });
-      res.json({
-        original_url: url,
-        short_url: short_url
-      });
-    }
-  });
+  if (!url) {
+    res.json({
+      error: 'invalid url'
+    })
+  } else {
+    const parsedUrl = new URL(url);
+    const host = parsedUrl.hostname;
+    dns.lookup(host, (err, address, family) => {
+      if (err) {
+        res.json({
+          error: 'invalid url'
+        });
+      } else {
+        const short_url = addresses.length + 1;
+        addresses.push({ original_url: url, short_url: short_url });
+        res.json({
+          original_url: url,
+          short_url: short_url
+        });
+      }
+    });
+  }
 });
 
 app.get('/api/shorturl/:id', (req, res) => {
-  const id = req.params.id;
-  res.redirect('https://www.google.com')
+  const id = parseInt(req.params.id);
+
+  const foundAddress = addresses.find((address) => address.short_url === id);
+
+  if (foundAddress) {
+    res.redirect(foundAddress.original_url);
+  } else {
+    res.status(404).json({
+      error: 'Short URL not found'
+    });
+  }
 });
+
+
 
 app.listen(port, function () {
   console.log(`Listening on port ${port}`);
