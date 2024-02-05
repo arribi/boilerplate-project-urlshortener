a2bb5e98bb8585478e1d20afad7bf94b06f78ba5
Jesús Arribi <arribi@boxer.local>
2024-02-05 12:19:34 +0000

    First attempt

--- index.js
+++ index.js
@@ -10,15 +10,36 @@ app.use(cors());
 
 app.use('/public', express.static(`${process.cwd()}/public`));
 
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.sendFile(process.cwd() + '/views/index.html');
 });
 
 // Your first API endpoint
-app.get('/api/hello', function(req, res) {
+app.get('/api/hello', function (req, res) {
   res.json({ greeting: 'hello API' });
 });
 
-app.listen(port, function() {
+app.post('/api/shorturl', (req, res) => {
+  const url = req.body.url;
+  dns.lookup(err, url) {
+    if (err) {
+      res.json({
+        error: 'invalid url'
+      });
+    } else {
+      res.json({
+        original_url: url,
+        short_url: 1
+      });
+    }
+  }
+});
+
+app.get('/api/shorturl/:id', (req, res) => {
+  const id = req.params.id;
+  res.redirect('https://www.google.com')
+});
+
+app.listen(port, function () {
   console.log(`Listening on port ${port}`);
 });
